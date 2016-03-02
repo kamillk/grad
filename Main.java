d7004e05a3930406ed6824e0babd4b1be5ec01c7
kamillk <kamillk@users.noreply.github.com>
2016-03-02 19:14:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import javax.swing.text.html.HTMLDocument;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,7 +16,7 @@ public class Main {
         currentUser = new char[100];
         currentItem = new char[100];
 
-        File file = new File("/Users/kamilla/Documents/Education/Graduate work/data.txt");
+        File file = new File("/Users/kamilla/Documents/Graduate work/data.txt");
 
         try(FileReader reader = new FileReader(file))
         {
@@ -63,6 +64,44 @@ public class Main {
                 index ++;
             }
 
+            //check
+            /*int ff = 0, ind = 0;
+            String item = "";
+            int n = UserItemArray.size();
+            String[] conformityArray = new String[n];
+            Iterator it = UserItemArray.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                String userName = pair.getKey().toString();
+                conformityArray[ind++] = userName;
+            }
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j <= i; ++j) {
+
+                    ff = 0;
+
+                    if (i != j) {
+
+                        String firstUsername = conformityArray[i];
+                        String secondUsername = conformityArray[j];
+                        Map<String, Integer> first = UserItemArray.get(firstUsername);
+                        Map<String, Integer> second = UserItemArray.get(secondUsername);
+                        Iterator iterator = uniqueItems.iterator();
+                        while (iterator.hasNext() && ff < 1) {
+                            item = iterator.next().toString();
+                            if (first.containsKey(item) && second.containsKey(item)) {
+                                ff++;
+                            }
+                        }
+                        if (ff >= 1) {
+                            System.out.println("i " + i);
+                            System.out.println("j " + j);
+                            System.out.println("item " + item);
+                        }
+                    }
+                }
+            }*/
+
             System.out.println("Please, choose the metric (1 - Manhattan metric, 2 - Euclidean metric, 3 - Cosine measure): ");
             Scanner sc = new Scanner(System.in);
             int metric = sc.nextInt();
@@ -85,12 +124,14 @@ public class Main {
             Object[] params = new Object[] { UserItemArray, uniqueItems, list };
             distanceMatrix = (double[][]) method.invoke(distanceClass, params);
 
-            for (int i = 0; i < 10; ++i) {
+            int n = UserItemArray.size();
+            for (int i = 0; i < 20; ++i) {
                 for (int j = 0; j <= i; ++j) {
                     System.out.print(distanceMatrix[i][j] + " ");
                 }
                 System.out.println("");
             }
+            //System.out.println(distanceMatrix[391][32]);
         }
         catch(IOException ex){
 
